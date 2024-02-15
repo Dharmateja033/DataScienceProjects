415ecf88360dd94be511b2a295f71c4533f8a7c5
Dharma <dharmateja.e.97@gmail.com>
2024-02-15 20:22:56 +0530

    Logger and Eception module developed

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import sys
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("Testing Custom Exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info('Testing Logging module')
     return "Pipeline established NOW!!"
 
 
 if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0',port=8080)
+    app.run(debug=False,host='0.0.0.0',port=8080)
