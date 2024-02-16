997f07ab05dd1b5e5f4db8946a1af5e547b8e6cf
Dharma <dharmateja.e.97@gmail.com>
2024-02-16 14:39:46 +0530

    creating constants,util,conf modules in housing

---
+++ housing/util/util.py
@@ -0,0 +1,13 @@
+import yaml
+from housing.exception import HousingException
+import os,sys
+
+
+
+#Function to read Yaml Files
+def read_yaml_file(file_path)->dict:
+    try:
+        with open(file_path,'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise HousingException(e,sys) from e
